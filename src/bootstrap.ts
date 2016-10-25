8b548ac249600467412827921d1441f305c41803
Quinntyne Brown <quinntynebrown@gmail.com>
2016-10-25 15:41:40 -0400

    refactor

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,9 +4,9 @@ import { environment } from "./environment";
 
 declare var jwplayer;
 
-export const bootstrap = (root: HTMLElement, key: string, storageKey, isDebug = false) => {         
+export const bootstrap = (root: HTMLElement, key: string, storeKey, isDebug = false) => {         
     jwplayer.key = key;       
-    Store.key = storageKey;
+    Store.key = storeKey;
     environment.isDebug = isDebug;
     const elements: NodeList = root.querySelectorAll('div[jw-player]');    
     for (var i = 0; i < elements.length; i++) {
